660f12f766c4e2a206473196a7a22bae4a8c86b3
null <94782486+kapdon@users.noreply.github.com>
2024-05-22 18:16:30 -0700

    3.9 prep
    make aki install dir easier to change for upcoming 3.9

--- entrypoint.alpine.sh
+++ entrypoint.alpine.sh
@@ -15,6 +15,8 @@ SPT_IP=${SPT_IP:-0.0.0.0}
 SPT_BACKEND_IP=${BACKEND_IP:-$(curl -s4 ipv4.icanhazip.com)}
 SPT_LOG_REQUESTS=${LOG_REQUESTS:-true}
 NEW_SERVER_NAME=${SERVER_NAME:-SIT $SIT_VERSION}
+SPT_CONFIG_PATH=Aki_Data/Server/config
+#SPT_CONFIG_PATH=SPT_Data/Server/config 3.9
 
 #DBUG
 echo "DEBUG build: $buildver, HEADLESS: $HEADLESS, UPDATE: $UPDATE"
@@ -34,25 +36,27 @@ sit_setup() {
   cd /opt/server
   chown $(id -u):$(id -g) ./* -Rf
 
-  sed -ir 's/"ip": .*,/"ip": "'$SPT_IP'",/' /opt/server/Aki_Data/Server/configs/http.json
-  MOD_IP=$(sed -n 's/.*"ip": "\(.*\)",/\1/p' /opt/server/Aki_Data/Server/configs/http.json)
+  sed -ir 's/"ip": .*,/"ip": "'$SPT_IP'",/' /opt/server/$SPT_CONFIG_PATH/http.json
+  MOD_IP=$(sed -n 's/.*"ip": "\(.*\)",/\1/p' /opt/server/$SPT_CONFIG_PATH/http.json)
   echo "SPT_IP: $MOD_IP, updating http.json"
 
-  sed -ir 's/"backendIp": .*,/"backendIp": "'$SPT_BACKEND_IP'",/' /opt/server/Aki_Data/Server/configs/http.json
-  MOD_BIP=$(sed -n 's/.*"backendIp": "\(.*\)",/\1/p' /opt/server/Aki_Data/Server/configs/http.json)
+  sed -ir 's/"backendIp": .*,/"backendIp": "'$SPT_BACKEND_IP'",/' /opt/server/$SPT_CONFIG_PATH/http.json
+  MOD_BIP=$(sed -n 's/.*"backendIp": "\(.*\)",/\1/p' /opt/server/$SPT_CONFIG_PATH/http.json)
   echo "BACKEND_IP: $MOD_BIP, updating http.json"
 
-  sed -ir "s/\"serverName\": \".*\"/\"serverName\": \"$NEW_SERVER_NAME\"/" /opt/server/Aki_Data/Server/configs/core.json
-  MODIFIED_NAME=$(sed -n 's/.*"serverName": "\([^"]*\)".*/\1/p' /opt/server/Aki_Data/Server/configs/core.json)
+  sed -ir "s/\"serverName\": \".*\"/\"serverName\": \"$NEW_SERVER_NAME\"/" /opt/server/$SPT_CONFIG_PATH/core.json
+  MODIFIED_NAME=$(sed -n 's/.*"serverName": "\([^"]*\)".*/\1/p' /opt/server/$SPT_CONFIG_PATH/core.json)
   echo "serverName: $MODIFIED_NAME, updating core.json"
 
-  sed -ir 's/"logRequests": .*,/"logRequests": '"$SPT_LOG_REQUESTS"',/' /opt/server/Aki_Data/Server/configs/http.json
-  MOD_LOGQ=$(sed -n 's/.*"logRequests": \(.*\),/\1/p' /opt/server/Aki_Data/Server/configs/http.json)
+  sed -ir 's/"logRequests": .*,/"logRequests": '"$SPT_LOG_REQUESTS"',/' /opt/server/$SPT_CONFIG_PATH/http.json
+  MOD_LOGQ=$(sed -n 's/.*"logRequests": \(.*\),/\1/p' /opt/server/$SPT_CONFIG_PATH/http.json)
   echo "logRequests: $MOD_LOGQ, updating http.json"
 
   # remove previous install.log n boot server once in bg to generate files.
   rm /opt/server/install.log
   screen -L -Logfile "install.log" -d -m -S AkiServer ./Aki.Server.exe
+  # 3.9 upcoming
+  # screen -L -Logfile "install.log" -d -m -S AkiServer ./SPT.Server.exe
 	while [ ! -f "/opt/server/user/mods/SITCoop/config/coopConfig.json" ]; do
 		sleep 10  # sleep till coopConfig.json is generated
 	done
